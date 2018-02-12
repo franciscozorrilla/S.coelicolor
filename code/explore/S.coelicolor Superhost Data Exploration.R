bc96f988706372b43411cd719bde4c835f7afdbe
franciscozorrilla <35606471+franciscozorrilla@users.noreply.github.com>
2018-02-12 14:28:32 +0100

    Update

--- code/explore/S.coelicolor Superhost Data Exploration.R
+++ code/explore/S.coelicolor Superhost Data Exploration.R
@@ -17,6 +17,7 @@ library(biomaRt)
 library(Glimma)
 library(piano)
 library(snowfall)
+library(readr) #Need this library to load/read .csv files
 
 #CLEAR ENVIRONMENT
 
@@ -24,13 +25,12 @@ rm(list = ls())
 
 #IMPORT DATA
 
-setwd("~/Superhost Individual Project/data/raw_internal/RNAseq") #Go to data folder
+setwd("C:/Users/zorrilla/Documents/GitHub/S.coelicolor/data/raw_internal/RNAseq") #Go to data folder
 
-library(readr) #Need this library to load/read .csv files
 rawCountData_M1152 <- read_csv("rawCountData_M1152.csv") #These two lines give an error/warning message about a missing
 rawCountData_M145 <- read_csv("rawCountData_M145.csv")   #column name, but they seem to work fine.
 
-setwd("~/Superhost Individual Project/code/explore") #Go back to code explore folder
+setwd("C:/Users/zorrilla/Documents/GitHub/S.coelicolor/code/explore") #Go back to code explore folder
 
 #CHECK DATA
 
@@ -70,183 +70,129 @@ x <- DGEList(counts = rawCounts, genes = rownames(rawCounts))
 group <- factor(rep(1:9,6),levels = c("1","2","3","4","5","6","7","8","9"))
 x$samples$group <- group
 
+strain <- factor(c(rep("WT",27),rep("MUT",27)),levels = c("WT","MUT")) 
+x$samples$strain <- strain #should I create another DGE list with same data 
+                           #and make the group be according to strain type instead of creating a new field?
+
 #FILTERING STEP 
 
 cpm <- cpm(x) 
 lcpm <- cpm(x, log = T) #use logcpm for filtering, not normalization
 keep.exprs <- rowSums(cpm > 1) >= 3 #Here we apply a filter that states that for each
                                     # gene at least 3 of the samples should have a CPM value higher than 1.
-x <- x[keep.exprs,, keep.lib.sizes = FALSE]
-dim(x)
+
+x <- x[keep.exprs,, keep.lib.sizes = FALSE] #Consider changing this filtering step to account for fact that
+dim(x)                                      #we know that gene expression in the MUT will be low for the
+                                            #deleted genes. ie: filter based on WT and non-deleted MUT genes
 
 #NORMALIZATION USING TMM
 
-# To properly normalize for library size we use TMM normalization, as discussed in the lectures.
 x <- calcNormFactors(x, method = "TMM")
-# If we look at the normalization factors, we see that the effect of TMM normalization was
-# quite mild, which might not be too surprising with similar library sizes for each sample
-x$samples
+lcpmTMM = cpm(x,normalized.lib.sizes= TRUE, log=TRUE) #not necessary
 
-#================== 4. Unsupervised clustering =======EDIT THIS SECTION====================
+# Visualize effect of TMM, so do I need to use CPM to incorporate TMM?? I thought it was an alternative
+lcpm2TMM = cpm(x,normalized.lib.sizes= TRUE, log=TRUE)
+lcpm2 = cpm(x,normalized.lib.sizes= FALSE, log=TRUE)
+View(lcpm2)
+View(lcpm2TMM)
+boxplot(lcpm2)
+title(main = "logCPM")
+boxplot(lcpm2TMM)
+title(main = "logCPM w/TMM")
+remove(lcpm2)
+remove(lcpm2TMM)
 
-# Good to see if there are any outliers in the data:
+# PCA -ED
+
+# Good to see if there are any outliers in the data: cant really tell from plot A tho
 par(mfrow = c(1, 2))
 col.group <- group
-levels(col.group) <- brewer.pal(nlevels(col.group), "Set1") #set colors to WT, look at col.group
+levels(col.group) <- brewer.pal(nlevels(col.group), "Set1")
 col.group <- as.character(col.group)
-plotMDS(lcpm, labels = group, col = col.group)
-title(main = "A. Dimensions 1&2")
-plotMDS(lcpm, labels = group, col = col.group, dim = c(3, 4))
-title(main = "B. Dimensions 3&4")
-
-# This gives question: is one of the ref and one of the pH swapped?
-# Something that can explain separation in dim 3? (same reactors?)
+plotMDS(lcpm, labels = group, col = col.group)   #is multidimensional scaling = PCA??
+title(main = "A. Color based on timegroup")
+col.group = c(rep('#E41A1C',27),rep('#377EB8',27))
+plotMDS(lcpm, labels = group, col = col.group, dim = c(1, 2)) 
+title(main = "B. Color based on strain")
 
 # Look interactively with Glimma
-glMDSPlot(lcpm, labels = group, groups = x$samples[, 2], folder = "scratch/glimma",
-          launch = T)
+glMDSPlot(lcpm, labels = strain, groups = x$samples$strain,
+          launch = T) #plot is good but table is kind of wonky/doesnt display information besides strain
 
-# Let's leave all samples in for now, and look what the rest of the analysis has to offer.
+# PCA - Benj
 
-#DATA EXPLORATION, ADD BOXPLOTS COMPARING TIME POINTS
+require(ade4)
 
-boxplot(logCPM[,],ylab="logCPM") #Should this be centered around zero like in the bioinformatics ex.3?
+#Transpose the data
+countsForPCA = t(lcpmTMM)
 
-boxplot(log(filtCounts+1),ylab="log(rawgeneExp+1)") #Comparing this to the logCPM plot shows effect of normalization
+#Perform PCA on the counts
+pcar <- dudi.pca(countsForPCA, center = TRUE, scale = FALSE, scannf = FALSE, nf=10)
 
-rawChangeInExpression =rowSums(rawCountData_M145) - rowSums(rawCountData_M1152) #look at min and max of raw
-which(rawChangeInExpression==max(rawChangeInExpression))
+#Check how much of the total variance each principal component accounts for:
+var <- pcar$eig/sum(pcar$eig)*100
+plot(var, type = 'b')
 
-#interesting gene (SCO6282) Identified by rawChangeInExpression plot above CHANGED AFTER FILTRATION
-intGene1WT = t(logCPM["SCO6282",1:27])
-intGene1MUT = t(logCPM["SCO6282",28:54])
-intGene1logCPM = cbind(intGene1WT,intGene1MUT)
-boxplot(intGene1logCPM, ylab = 'logCPM(Gene Expression)') #still differentially expressed after normalization
-#https://www.ncbi.nlm.nih.gov/pmc/articles/PMC3293853/ ----PAPER THAT MENTIONS THIS GENE/ENZYME
+#Plot factorial map with representation of observations in the 1st 2 components:
+s.class(pcar$li[,1:2], strain, cpoint = 1, col = c('blue','red'))
 
-which(rawChangeInExpression==min(rawChangeInExpression)) #find most upregulated in MUT
-intGene2WT = t(logCPM["SCO4296",1:27])
-intGene2MUT = t(logCPM["SCO4296",28:54])
-intGene2logCPM = cbind(intGene2WT,intGene2MUT)
-boxplot(intGene2logCPM, ylab = 'logCPM(Gene Expression)') #not very differentially expressed after normalization
+####################################################################
+# Linear models
 
-#LINEAR MODEL TO DETERMINE DE OF GENE SCO6282 & SCO4296
+      # strainTest <- c(rep(1,3),rep(0,3))
+      # t1 <- rep(c(1,0,0,0,0,0,0,0,0),6)
+      # t2 <- rep(c(0,1,0,0,0,0,0,0,0),6)
+      # t3 <- rep(c(0,0,1,0,0,0,0,0,0),6)
+      # t4 <- rep(c(0,0,0,1,0,0,0,0,0),6)
+      # t5 <- rep(c(0,0,0,0,1,0,0,0,0),6)
+      # t6 <- rep(c(0,0,0,0,0,1,0,0,0),6)
+      # t7 <- rep(c(0,0,0,0,0,0,1,0,0),6)
+      # t8 <- rep(c(0,0,0,0,0,0,0,1,0),6)
+      # t9 <- rep(c(0,0,0,0,0,0,0,0,1),6)
+      
+      # design <- model.matrix(~ strain * t1 * t2 * t3 * t4 * t5 * t6 * t7 * t8 * t9)
 
-type = c(rep("WT",27), rep("MUT",27)) #create type variable for distinguishing between WT and MUT
-type = as.factor(type)
-type = relevel(type, ref = "WT")
+t1 <- rep(c(1,0,0,0,0,0,0,0,0),6)
+t2 <- rep(c(0,1,0,0,0,0,0,0,0),6)
+t3 <- rep(c(0,0,1,0,0,0,0,0,0),6)
+t4 <- rep(c(0,0,0,1,0,0,0,0,0),6)
+t5 <- rep(c(0,0,0,0,1,0,0,0,0),6)
+t6 <- rep(c(0,0,0,0,0,1,0,0,0),6)
+t7 <- rep(c(0,0,0,0,0,0,1,0,0),6)
+t8 <- rep(c(0,0,0,0,0,0,0,1,0),6)
+t9 <- rep(c(0,0,0,0,0,0,0,0,1),6)
+design <- model.matrix(~ strain * ( t2 + t3 + t4 + t5 + t6 + t7 + t8 + t9)) #i dont understand what all the terms in the design matrix mean
+                                                          #when i include all of the time terms, specifically the ones with ":" in their name
+design <- model.matrix(~ strain + t2 + t3 + t4 + t5 + t6 + t7 + t8 + t9) 
 
-SCO6282data=cbind(type,t(logCPM["SCO6282",]))
-SCO6282data=data.frame(SCO6282data)
-fit1= lm(SCO6282~type, data=SCO6282data)
+design <- model.matrix(~ strain ) #do i need to make a design model for each different time term or can i include them all in one?
+design
 
-SCO4296data=cbind(type,t(logCPM["SCO4296",]))
-SCO4296data=data.frame(SCO4296data)
-fit2= lm(SCO4296~type, data=SCO4296data)
+x <- estimateDisp(x, design) #give me warning when i have all time interaction terms in design matrix
 
-#LINEAR MODEL FOR ALL GENES
+# Fit data to linear model
+mfit <- glmQLFit(x, design)
+# Inspect the coefficients that are found
+head(mfit$coefficients)
 
-pvalsFit1 = c()
-estimatesFit1 = c()
+# Perform DE analysis per factor
+de <- glmLRT(mfit,coef = 2)
+topTags(de)
+tt <- topTags(de, n = Inf)
+genes <- tt$table$genes[tt$table$FDR < 0.001]
+plotMDS(lcpm[genes, ], labels = group, col = col.group) #just using the model with strain type, this plot looks exactly like the PCA plot 
+                                    #previously obtained, so what was the point of going through the linear model?
 
-for (z in 1:7852){ #use edgeR and limma
-  geneData = cbind(type,t(logCPM[z,]))
-  geneData = data.frame(geneData)
-  fit1X = lm(geneData[,2]~type, data=geneData)
-  estimatesFit1[z] = summary(fit1X)$coeff[2]
-  pvalsFit1[z] = summary(fit1X)$coeff[8]
-}
+#look at interaction terms
 
-sigGeneNames = row.names(logCPM)
-fitDataFrame = cbind(pvalsFit1,estimatesFit1)
-fitDataFrame = data.frame(fitDataFrame)
-row.names(fitDataFrame) = sigGeneNames
+#get uniprot gs list
+##################################################################
 
-pvalsFit1Adj = p.adjust(fitDataFrame[,1], method = "fdr", n = 7852)
-fitDataFrame = cbind(fitDataFrame,pvalsFit1Adj)
+#benmodel
 
-sigPvalsFit1idx = pvalsFit1Adj<0.05
-#sigPvalsFit1 = pvalsFit1Adj[sigPvalsFit1idx] CAN DELETE
-sigPvalsFit1DataFrame = fitDataFrame[fitDataFrame[,3]<0.05,]
+v <- voom(x,design,plot=T)#warning message? gives warning when more than one time term included in model
+#also does VOOM do the same thing as estimateDisp?
 
-#sigEsimates1 = fitDataFrame[sigPvalsFit1idx,2] CAN DELETE
-
-#sigGeneFit1DataFrame = cbind(sigPvalsFit1,sigEsimates1) CAN DELETE
-#sigGeneFit1DataFrame = data.frame(sigGeneFit1DataFrame) CAN DELETE
-
-fit1up = sigPvalsFit1DataFrame[sigPvalsFit1DataFrame[,2]>0,]
-fit1down = sigPvalsFit1DataFrame[sigPvalsFit1DataFrame[,2]<0,]
-
-#HEAT MAP OF TOP 100 SIG DE GENES
-
-#source("https://bioconductor.org/biocLite.R")
-#biocLite("edgeR")
-
-sortedPvals = sort(fitDataFrame[,3],decreasing=FALSE)
-hunnedCutoffIdx = sortedPvals[100]
-hunnedFrame = fitDataFrame[fitDataFrame[,3]<=hunnedCutoffIdx,]
-hunnedGenes = row.names(hunnedFrame)
-tlogCPM = t(logCPM)
-xsig = tlogCPM[,hunnedGenes]
-library(RColorBrewer)
-library(gplots)
-mypalette = brewer.pal(11,"RdYlBu")
-morecols = colorRampPalette(mypalette)
-mycols=rev(morecols(255))
-column.cols=c("purple","orange")[type]
-pdf("top100sigGenesHeatmap.pdf",height=9,width=6)
-heatmap.2(t(xsig),trace='none',col=mycols,main='The 100 most significant
-          genes',ColSideColors=column.cols)
-dev.off()
-
-#LINEAR MODEL BY TIME POINT
-
-#Columns 1-9 contain 9 time points P(6,14,18,22,26,30,34,38,42) of F516 sample (WT)
-#Columns 10-18 contain 9 time points P(6,14,18,22,26,30,34,38,42) of F517 sample (WT)
-#Columns 19-27 contain 9 time points P(6,14,18,22,26,30,34,38,42) of F518 sample (WT)
-#Columns 28-36 contain 9 time points P(18,26,30,34,38,42,46,50,51) of F519 sample (MUT)
-#Columns 37-45 contain 9 time points P(18,26,30,34,38,42,46,50,51) of F521 sample (MUT)
-#Columns 46-54 contain 9 time points P(18,26,30,34,38,42,46,50,51) of F522 sample (MUT)
-
-timePt = c(rep("1",3),rep("2",3),rep("3",3),rep("4",3),rep("5",3),rep("6",3),
-           rep("7",3),rep("8",3),rep("9",3),rep("1",3),rep("2",3),rep("3",3),
-           rep("4",3),rep("5",3),rep("6",3),rep("7",3),rep("8",3),rep("9",3))
-
-
-#timePt = c(rep("P6",3),rep("P14",3),rep("P18",3),rep("P22",3),rep("P26",3),rep("P30",3),
- #          rep("P34",3),rep("P38",3),rep("P42",3),rep("P18",3),rep("P26",3),rep("P30",3),
-  #         rep("P34",3),rep("P38",3),rep("P42",3),rep("P46",3),rep("P50",3),rep("P51",3))
-
-
-
-typePvalsFit2 = c()
-timePtPvalsFit2 = c()
-typeEstimatesFit2 = c()
-timePtEstimatesFit2 = c()
-timePt =as.factor(timePt)
-
-for (z in 1:7852){
-  geneData = cbind(type,timePt, t(logCPM[z,]))
-  geneData = data.frame(geneData)
-  fit2X = lm(geneData[,3]~type+timePt, data=geneData)
-  typePvalsFit2 = c()
-  timePtPvalsFit2 = c()
-  typeEstimatesFit2 = c()
-  timePtEstimatesFit2 = c()
-  estimatesFit1[z] = summary(fit1X)$coeff[2]
-  pvalsFit1[z] = summary(fit1X)$coeff[8]
-}
-
-#PCA
-
-pca=prcomp(tlogCPM)
-summary(pca)
-
-plot(pca$x[,1],pca$x[,2],col =column.cols, xlab = "PC1",ylab = "PC2")
-title("PCA Scatter Plot")
-
-plot(pca$x[,1],pca$x[,3],col =column.cols, xlab = "PC1",ylab = "PC3")
-title("PCA Scatter Plot")
-
-plot(pca$x[,2],pca$x[,3],col =column.cols, xlab = "PC2",ylab = "PC3")
-title("PCA Scatter Plot")
+fit <- lmFit(v,design) #this takes forever when i include all time terms in model
+eb  <- eBayes(fit)
+topTable(eb,coef = 2) #what does coef refer to? column of design matrix, we are interested in the ones after the intercepet right?
