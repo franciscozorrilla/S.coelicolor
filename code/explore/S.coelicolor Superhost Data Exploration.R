7a5d7ac832009f5ed626ed426cd80409b205f5d4
franciscozorrilla <35606471+franciscozorrilla@users.noreply.github.com>
2018-03-05 15:06:05 +0100

    GSA + Linear Model

--- code/explore/S.coelicolor Superhost Data Exploration.R
+++ code/explore/S.coelicolor Superhost Data Exploration.R
@@ -5,10 +5,8 @@
 # PACKAGES
 source("https://bioconductor.org/biocLite.R")
 biocLite(c("limma", "edgeR", "piano", "biomaRt", "Glimma"))
-install.packages(c('tidyverse', 'RColorBrewer'))
+install.packages(c('tidyverse', 'RColorBrewer','snowfall','tibble'))
 
-# After installing the software the first time, load the R-packages that we need. This you need
-# to do every time you start a new R session.
 library(limma)
 library(edgeR)
 library(tidyverse) # Collection of useful R-packages
@@ -18,6 +16,7 @@ library(Glimma)
 library(piano)
 library(snowfall)
 library(readr) #Need this library to load/read .csv files
+library(tibble)
 
 #CLEAR ENVIRONMENT
 
@@ -36,7 +35,6 @@ setwd("C:/Users/zorrilla/Documents/GitHub/S.coelicolor/code/explore") #Go back t
 
 head(rawCountData_M1152) #Just to make sure data was properly imported/nothing weird happened
 head(rawCountData_M145)
-
 dim(rawCountData_M1152) #Check dimensions of count matrices, should be equal
 dim(rawCountData_M145)
 
@@ -44,14 +42,13 @@ dim(rawCountData_M145)
 
 row.names(rawCountData_M1152)=rawCountData_M1152$X1 #Set row names to gene names. These two lines give a warning
 row.names(rawCountData_M145)=rawCountData_M145$X1   #message about tibble depreciation?? They work fine.
-
 rawCountData_M1152$X1 <- NULL # delete columns with gene names
 rawCountData_M145$X1 <- NULL
-
 rawCounts = cbind(rawCountData_M145,rawCountData_M1152) #Create single dataframe combining raw counts
                                                         #of the two strains
+remove(rawCountData_M1152,rawCountData_M145)
 
-view(rawCounts) #Check that new data frame looks good
+View(rawCounts) #Check that new data frame looks good
 
     #Columns 1-9 contain 9 time points P(6,14,18,22,26,30,34,38,42) of F516 sample (WT)
     #Columns 10-18 contain 9 time points P(6,14,18,22,26,30,34,38,42) of F517 sample (WT)
@@ -63,44 +60,46 @@ view(rawCounts) #Check that new data frame looks good
 # Move data into DGEList, which can store additional metadata
 x <- DGEList(counts = rawCounts, genes = rownames(rawCounts))
 
-
 # Add the grouping information (and enforce the order of the samples).
-
 
 group <- factor(rep(1:9,6),levels = c("1","2","3","4","5","6","7","8","9"))
 x$samples$group <- group
-
 strain <- factor(c(rep("WT",27),rep("MUT",27)),levels = c("WT","MUT")) 
-x$samples$strain <- strain #should I create another DGE list with same data 
-                           #and make the group be according to strain type instead of creating a new field?
+x$samples$strain <- strain 
+
+#NORMALIZATION USING TMM
+
+x <- calcNormFactors(x, method = "TMM") #do before filtering, violates assumption that downreg genes=upreg genes
+#lcpmTMM = cpm(x,normalized.lib.sizes= TRUE, log=TRUE) #not necessary, non standard, need to justify reason for donig this
+
 
 #FILTERING STEP 
 
-cpm <- cpm(x) 
-lcpm <- cpm(x, log = T) #use logcpm for filtering, not normalization
+cpm <- cpm(x,normalized.lib.sizes= TRUE) #i think cpm() uses norm.lib.sizes by default
+#cpm2 <- cpm(x) #should there be a diffrence between cpm and cpm2?
+
+#lcpm <- cpm(x,normalized.lib.sizes= TRUE, log=TRUE) #use logcpm for filtering, not normalization. Should i use this?
+
 keep.exprs <- rowSums(cpm > 1) >= 3 #Here we apply a filter that states that for each
                                     # gene at least 3 of the samples should have a CPM value higher than 1.
+
+        #is it better to filter using lcpm or cpm?
 
 x <- x[keep.exprs,, keep.lib.sizes = FALSE] #Consider changing this filtering step to account for fact that
 dim(x)                                      #we know that gene expression in the MUT will be low for the
                                             #deleted genes. ie: filter based on WT and non-deleted MUT genes
 
-#NORMALIZATION USING TMM
-
-x <- calcNormFactors(x, method = "TMM")
-lcpmTMM = cpm(x,normalized.lib.sizes= TRUE, log=TRUE) #not necessary
 
 # Visualize effect of TMM, so do I need to use CPM to incorporate TMM?? I thought it was an alternative
-lcpm2TMM = cpm(x,normalized.lib.sizes= TRUE, log=TRUE)
+lcpmTMM = cpm(x,normalized.lib.sizes= TRUE, log=TRUE)
 lcpm2 = cpm(x,normalized.lib.sizes= FALSE, log=TRUE)
-View(lcpm2)
-View(lcpm2TMM)
 boxplot(lcpm2)
 title(main = "logCPM")
-boxplot(lcpm2TMM)
+boxplot(lcpmTMM)
 title(main = "logCPM w/TMM")
 remove(lcpm2)
-remove(lcpm2TMM)
+
+#note, if using CPM or logCPM make sure it is recalculated after filtering
 
 # PCA -ED
 
@@ -109,14 +108,17 @@ par(mfrow = c(1, 2))
 col.group <- group
 levels(col.group) <- brewer.pal(nlevels(col.group), "Set1")
 col.group <- as.character(col.group)
-plotMDS(lcpm, labels = group, col = col.group)   #is multidimensional scaling = PCA??
+x11();plotMDS(lcpmTMM, labels = group, col = col.group)   #use TMM not lcmp, use spearman
 title(main = "A. Color based on timegroup")
 col.group = c(rep('#E41A1C',27),rep('#377EB8',27))
-plotMDS(lcpm, labels = group, col = col.group, dim = c(1, 2)) 
+x11();plotMDS(lcpmTMM, labels = group, col = col.group, dim = c(1, 2)) 
 title(main = "B. Color based on strain")
+            #one of the time point 9 samples from the mutant strain looks like it may be an outlier
+            #especially considering the general "walk" pattern we see where the smaller numbers tend to be
+            #lower than the bigger numbers
 
 # Look interactively with Glimma
-glMDSPlot(lcpm, labels = strain, groups = x$samples$strain,
+glMDSPlot(lcpmTMM, labels = strain, groups = x$samples$strain,
           launch = T) #plot is good but table is kind of wonky/doesnt display information besides strain
 
 # PCA - Benj
@@ -134,10 +136,11 @@ var <- pcar$eig/sum(pcar$eig)*100
 plot(var, type = 'b')
 
 #Plot factorial map with representation of observations in the 1st 2 components:
-s.class(pcar$li[,1:2], strain, cpoint = 1, col = c('blue','red'))
+x11();s.class(pcar$li[,1:2], strain, cpoint = 1, col = c('blue','red'))
+s.class(pcar$li[,2:3], strain, cpoint = 1, col = c('blue','red'))
 
-####################################################################
-# Linear models
+
+# LINEAR MODELS
 
       # strainTest <- c(rep(1,3),rep(0,3))
       # t1 <- rep(c(1,0,0,0,0,0,0,0,0),6)
@@ -150,7 +153,10 @@ s.class(pcar$li[,1:2], strain, cpoint = 1, col = c('blue','red'))
       # t8 <- rep(c(0,0,0,0,0,0,0,1,0),6)
       # t9 <- rep(c(0,0,0,0,0,0,0,0,1),6)
       
-      # design <- model.matrix(~ strain * t1 * t2 * t3 * t4 * t5 * t6 * t7 * t8 * t9)
+      # design <- model.matrix(~ strain ) #only strain, no time component 
+      # design <- model.matrix(~ strain + t2 + t3 + t4 + t5 + t6 + t7 + t8 + t9) #no interaction
+      # design <- model.matrix(~ strain * t1 * t2 * t3 * t4 * t5 * t6 * t7 * t8 * t9) #dont use this, it creates
+                                                                                      #weird interaction terms
 
 t1 <- rep(c(1,0,0,0,0,0,0,0,0),6)
 t2 <- rep(c(0,1,0,0,0,0,0,0,0),6)
@@ -161,38 +167,76 @@ t6 <- rep(c(0,0,0,0,0,1,0,0,0),6)
 t7 <- rep(c(0,0,0,0,0,0,1,0,0),6)
 t8 <- rep(c(0,0,0,0,0,0,0,1,0),6)
 t9 <- rep(c(0,0,0,0,0,0,0,0,1),6)
-design <- model.matrix(~ strain * ( t2 + t3 + t4 + t5 + t6 + t7 + t8 + t9)) #i dont understand what all the terms in the design matrix mean
-                                                          #when i include all of the time terms, specifically the ones with ":" in their name
-design <- model.matrix(~ strain + t2 + t3 + t4 + t5 + t6 + t7 + t8 + t9) 
-
-design <- model.matrix(~ strain ) #do i need to make a design model for each different time term or can i include them all in one?
+design <- model.matrix(~ strain * ( t2 + t3 + t4 + t5 + t6 + t7 + t8 + t9)) 
 design
 
-x <- estimateDisp(x, design) #give me warning when i have all time interaction terms in design matrix
+#daniel: try making design matrix with one term for time, with differnt levels
+  #if we do this then we cant get interaction terms right?
 
-# Fit data to linear model
-mfit <- glmQLFit(x, design)
-# Inspect the coefficients that are found
-head(mfit$coefficients)
+x <- estimateDisp(x, design) # Calculate dispersion using x and design model
+mfit <- glmQLFit(x, design) # Fit data to linear model
+head(mfit$coefficients) # Inspect the coefficients that are found
 
 # Perform DE analysis per factor
-de <- glmLRT(mfit,coef = 2)
-topTags(de)
-tt <- topTags(de, n = Inf)
+de <- glmLRT(mfit,coef = 2) #is this legal to put groups of coefficients at the same time? or do i have to do 1 at a time
+                                #how does choice of coeffcients influence pvalues?
+
+topTags(de) #is the fold change relative to MUT or WT?
+tt <- topTags(de, n = Inf) #store top DE genes in list tt
 genes <- tt$table$genes[tt$table$FDR < 0.001]
-plotMDS(lcpm[genes, ], labels = group, col = col.group) #just using the model with strain type, this plot looks exactly like the PCA plot 
-                                    #previously obtained, so what was the point of going through the linear model?
+
+#plotMDS(lcpmTMM[genes, ], labels = group, col = col.group) #not needed
 
 #look at interaction terms
 
-#get uniprot gs list
-##################################################################
+################################do tSNE
 
-#benmodel
+library(Rtsne)
 
-v <- voom(x,design,plot=T)#warning message? gives warning when more than one time term included in model
-#also does VOOM do the same thing as estimateDisp?
 
-fit <- lmFit(v,design) #this takes forever when i include all time terms in model
-eb  <- eBayes(fit)
-topTable(eb,coef = 2) #what does coef refer to? column of design matrix, we are interested in the ones after the intercepet right?
+##### GSA
+
+## Download GO terms from Uniprot, S. coelicolor 'proteome': http://www.uniprot.org/uniprot/?query=proteome:UP000001973
+## Use columns 'Gene names (ordered locus)' and 'Gene ontology (GO)'
+## Save as tab-delimited
+GO<-read.delim('uniprot-proteome%3AUP000001973.tab')
+GO<-GO[,-1] # Remove 'Entry' column
+GO<-GO[!GO$Gene.names...ordered.locus..=='',] # Remove rows without gene name
+colnames(GO)<-c('gene','GO') # Rename columns
+
+# install.packages('splitstackshape') # Run this installation command if the package is not installed yet.
+library(splitstackshape)
+GO<-cSplit(GO,"GO",sep=';',direction='long') # Split the GO column, separated by ';', and make a long list
+head(GO)
+
+library(piano)
+GO<-loadGSC(GO) #ignore warning?
+
+fc <- subset(tt$table,select = "logFC")
+p <- subset(tt$table,select = "FDR")
+
+##############BEN
+resGSA  <- runGSA(geneLevelStats=p, #We supply our p-values
+                  directions=fc,    #We supply the fold-changes
+                  geneSetStat="reporter", #We decided the method
+                  signifMethod="geneSampling", #We choose the statistical distribution against which we measure our confidence
+                  adjMethod="fdr",  #We adjust the gene-set p-values for multiple testing
+                  gsc=GO, #We supply which gene-sets to test
+                  ncpus=4) #We decide the number of processor cores to run the calculations
+
+windows()
+GSAheatmap(resGSA,cutoff =10,adjusted = T,ncharLabel = Inf, cellnote='pvalue') #cutoff of 9 - 10 is readable
+#dev.off() 
+
+#################ED
+gsaRes <- runGSA(p, fc, gsc = GO, gsSizeLim = c(10, 400))
+
+x11();GSAheatmap(gsaRes, cutoff = 5, cellnote = "nGenes",ncharLabel = Inf)
+
+#BEN/ED GSAs give me different heatmaps: numbers are different(FCs?) and the GO terms
+#are also somewhat differrent. which one should i use and what parameters are causing
+#these differences? Why did Ed narrow down the gsSizeLim to c(10,400)?
+#How should I determine the parameters to set for geneSetStat and signifMethod?
+
+#how should i run GSA in order to determine DE expression between time points?
+#start from line de <- glmLRT(mfit,coef = 2) but change coef to an interaction term coef
